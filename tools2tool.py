ac30a4f38bbd36dfd66e00a512ff7cc22809eda0
Reema Gupta <reema.gupta@lmu.de>
2023-03-22 17:39:55 +0100

    small script to break master yaml file to tool specific files

---
+++ tools2tool.py
@@ -0,0 +1,22 @@
+import yaml
+
+def yaml_to_yamls():
+    with open("simtools/simtools.yaml", "r") as stream:
+        data = yaml.load(stream, Loader=yaml.FullLoader)
+        for simtool in data:
+            tool_name = list(simtool.keys())[0]
+            with open(f"simtools/{tool_name.replace(' ', '-')}.yaml", "w") as outfile:
+                val = [
+                    {"name": tool_name},
+                    {"website_url": ""},
+                    {"biological_level": ""},
+                    {"computing_scale": ""},
+                    {"interface_language": ""},
+                    {"model__description_language": ""},
+                ]
+                val.append(list(simtool.values())[0])
+                yaml.dump(val, outfile)
+
+                
+if __name__ == "__main__":
+    yaml_to_yamls()
