7cd1ee550fefb3200da9962c12a186b4c53227ce
Marcel Stimberg <marcel.stimberg@inserm.fr>
2023-04-07 11:50:52 +0200

    Refactor class and change general layout

--- src/project_browser.py
+++ src/project_browser.py
@@ -2,69 +2,73 @@ import data
 
 import panel as pn
 
-DATA = data.parse_files()
-VALUES = data.unique_entries(DATA)
+class SimSelect:
+    DATA = data.parse_files()
+    VALUES = data.unique_entries(DATA)
 
-def filtered_data(criteria):
-    '''
-    Filter the data based on the criteria
-    Args:
-        criteria: dict
-            The criteria to filter on
+    def filtered_data(self, criteria):
+        '''
+        Filter the data based on the criteria
+        Args:
+            criteria: dict
+                The criteria to filter on
 
-    Returns:
-        dict: The filtered data
-    '''
-    filtered = {}
-    for name, values in DATA.items():
-        for key, value in criteria.items():
-            if value and not set(value).issubset(values[key]):
-                break
-        else:
-            filtered[name] = values
-    return filtered
+        Returns:
+            dict: The filtered data
+        '''
+        filtered = {}
+        for name, values in SimSelect.DATA.items():
+            for key, value in criteria.items():
+                if value and not set(value).issubset(values[key]):
+                    break
+            else:
+                filtered[name] = values
+        return filtered
 
-# Create selection widgets
-os_select = pn.widgets.MultiChoice(name='Operating systems', options=VALUES["operating_system"])
-lang_select = pn.widgets.MultiChoice(name='Interface languages', options=VALUES["interface_language"])
-power_select = pn.widgets.MultiChoice(name='Computing power', options=VALUES["computing_scale"])
-model_select = pn.widgets.MultiChoice(name='Model type', options=VALUES["biological_level"])
-description_select = pn.widgets.MultiChoice(name='Model description language', options=VALUES["model__description_language"])
+    def update_cards(self, event):
+        filter_results = self.filtered_data({'operating_system': self.os_select.value,
+                                             'interface_language': self.lang_select.value,
+                                             'biological_level': self.model_select.value,
+                                             'computing_scale': self.power_select.value,
+                                             'model__description_language': self.description_select.value})
+        for simulator in self.simulators:
+            simulator.visible = simulator.name in filter_results
 
-# Basic layout
-header = pn.pane.Markdown("""
-# Simselect tool
-**Note: this is an early prototype and far from ready for general use**
-""")
-select_col = pn.Column(os_select, lang_select)
-select_co2 = pn.Column(power_select, model_select, description_select)
-select_row = pn.Row(select_col, select_co2)
+    def __init__(self):
+        # This is needed to make the app work in a notebook
+        pn.extension()
 
-# Create cards for all "simulators"
-layout = pn.FlexBox([])
+        self.template = pn.template.MaterialTemplate(title='SimSelect')
 
-# Due to watch=True, this function will be called whenever any of the widgets change
-# It updates the cards in place by assigning to layout.objects (another option would be to
-# change the cards' visibility, I guess)
-@pn.depends(os_select, lang_select, model_select, power_select, description_select, watch=True)
-def get_cards(os_select, lang_select, model_select, power_select, description_select):
+        # Create selection widgets
+        self.os_select = pn.widgets.MultiChoice(name='Operating systems', options=SimSelect.VALUES["operating_system"])
+        self.lang_select = pn.widgets.MultiChoice(name='Interface languages', options=SimSelect.VALUES["interface_language"])
+        self.power_select = pn.widgets.MultiChoice(name='Computing power', options=SimSelect.VALUES["computing_scale"])
+        self.model_select = pn.widgets.MultiChoice(name='Model type', options=SimSelect.VALUES["biological_level"])
+        self.description_select = pn.widgets.MultiChoice(name='Model description language', options=SimSelect.VALUES["model__description_language"])
 
-    cards = [pn.Card({key: value if isinstance(value, str) else ', '.join(sorted(value))
-             for key, value in DATA[name].items()},
-             title=name, collapsed=True)
-    for name in filtered_data({'operating_system': os_select,
-                               'interface_language': lang_select,
-                               'biological_level': model_select,
-                               'computing_scale': power_select,
-                               'model__description_language': description_select})]
-    layout.objects = cards
+        # Basic layout
+        self.template.header.append(pn.pane.Markdown("""
+        **Note: this is an early prototype and far from ready for general use**
+        """))
+        self.template.sidebar.append(self.os_select)
+        self.template.sidebar.append(self.lang_select)
+        self.template.sidebar.append(self.power_select)
+        self.template.sidebar.append(self.model_select)
+        self.template.sidebar.append(self.description_select)
 
-# This is needed to make the app work in a notebook
-pn.extension()
+        # Create "buttons" for all simulators
+        self.simulators = [pn.widgets.Button(name=name)
+                           for name in SimSelect.DATA.keys()]
+        layout = pn.FlexBox(*self.simulators)
+        self.template.main.append(layout)
+        self.os_select.param.watch(self.update_cards, 'value')
+        self.lang_select.param.watch(self.update_cards, 'value')
+        self.power_select.param.watch(self.update_cards, 'value')
+        self.model_select.param.watch(self.update_cards, 'value')
+        self.description_select.param.watch(self.update_cards, 'value')
 
-# Initially, display all simulators
-get_cards([], [], [], [], [])  # Select all simulators
-main = pn.Column(header, select_row, layout)
 
 if __name__.startswith("bokeh"):
-    main.servable()
+    sim_select = SimSelect()
+    sim_select.template.servable()
