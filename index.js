748c99485adfd33743a1af24f2d82f924544d0ee
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2025-02-27 15:06:36 +0100

    fix: Details pane: bring back "Back to simulators" and "Edit this description" button

--- index.js
+++ index.js
@@ -114,7 +114,35 @@ function showDetails(data, connected) {
             }
         }
         details_bottom.appendChild(flex_div);
+
+        // Back to simulators
+        back_p = document.createElement("p");
+        back_p.classList.add("mt-3");
+        back_button = document.createElement("a");
+        back_button.href = "#";
+        back_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/>
+                                    <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/>
+                                    </svg>&nbsp;Back to simulators`;
+        back_button.classList.add("btn", "btn-secondary");
+        back_button.onclick = function() { cy.nodes(`[id = '${data.id}']`).unselect(); cy.nodes("#simulators").select(); unhighlightNode(); };
+        back_p.appendChild(back_button);
+        details_bottom.appendChild(back_p);
+        // Edit footer
+        edit_p = document.createElement("p");
+        edit_p.classList.add("mt-3", "text-end");
+        edit_link = document.createElement("a");
+        edit_link.classList.add("link-secondary");
+        edit_link.href = `${REPO_URL}/edit/${GIT_BRANCH}/${DATA_FOLDER}/${data["short_name"].replaceAll(" ", "-")}.yaml`;
+        edit_link.innerHTML = "Edit this description on GitHub&nbsp;";
+        edit_link.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                                </svg>`;
+        edit_link.target = "_blank";
+        edit_p.appendChild(edit_link);
+        details_bottom.appendChild(edit_p);
     }
+
     // hide filter pane
     const filterPane = new bootstrap.Offcanvas('#filter_pane');
     // FIXME: not quite sure what is going on here, but sometimes the internal state is incorrect
